d922f249bb3ff56f3226ee4564d78260a933e227
YajasD <yajasdwivedi@gmail.com>
2017-07-27 06:25:57 +0000

    First commit

--- collections_hierarchy/base_classes.py
+++ collections_hierarchy/base_classes.py
@@ -1,24 +1,73 @@
-from .node import Node
+from .node import Node 
 
 
 class Sequenceable(object):
     def __init__(self):
-        pass
+        self.start = None #attribute
+        self.end = None
 
     def get_elements(self):
-        pass
+        result = []
+        current = self.start
+        while current is not None:
+            result.append(current.value)
+            current = current.next
+        return result
 
+# Node(X) > Node(Y) > Node(Z) > /
+#           ^         *
+#Node(A)
 
 class Appendable(object):
     def append(self, element):
-        pass
+        new_node = Node(element)
+        if self.start is None:
+            self.start = new_node
+            self.end = new_node
+        else:
+            self.end.next = new_node
+            self.end = new_node
+            
+        
+"""
+    def append(self, element): ###inefficient method
+        new_node = Node(element)
+        if self.start is None:
+            self.start = new_node
+        else:
+            previous_node = self.start
+            current_node = self.start.next
+            while current_node is not None:
+                previous_node = current_node
+                current_node = previous_node.next
+            
+            previous_node.next = new_node
+"""            
 
 
 class Popable(object):
     def pop(self):
-        pass
-
+        if self.start is None:
+            raise IndexError()
+        
+        first_node = self.start
+        self.start = self.start.next
+        return first_node.value
+        
+            
 
 class Pushable:
-    def push(self, element):
-        pass
+     def push(self, element):
+        new_node = Node(element)
+        if self.start is None:
+            self.start = new_node
+            self.end = new_node
+        else:
+            new_node.next = self.start
+            self.start = new_node
+#            new_node = self.start.next (WHY IS THIS WRONG?)
+#            self.start = new_node
+
+
+
+#sequence.start = n1
--- collections_hierarchy/node.py
+++ collections_hierarchy/node.py
@@ -1,3 +1,22 @@
 class Node(object):
     def __init__(self, value, next=None):
-        pass
+        self.value = value
+        self.next = next
+        
+n1 = Node('X')
+n2 = Node('Y')
+n3 = Node('Z')
+n4 = Node('A')
+
+n1.next = n2
+n2.next = n3
+n3.next = n4
+
+start = n1
+current = start
+
+while current is not None:
+    print(current.value)
+    current = current.next
+    
+
